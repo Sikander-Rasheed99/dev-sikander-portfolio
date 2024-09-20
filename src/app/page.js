23557b0851b3387eda86a99813cb4a743be2c707
Sikander-Rasheed99 <sikanderrasheed39@gmail.com>
2024-09-21 03:43:30 +0500

    err solved

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 
+
 export default function Home() {
   return (
     <>
